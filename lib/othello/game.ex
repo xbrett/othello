2bab555fd45928bbd9f3df96decc6c596615082f
Binh <lebinhb98@gmail.com>
2019-02-22 00:11:28 -0500

    refactored game

--- lib/othello/game.ex
+++ lib/othello/game.ex
@@ -15,7 +15,9 @@ defmodule Othello.Game do
 			turn: "black",
 			player1: "",
 			player2: "",
-			status: "waiting"
+			status: "waiting",
+			pcsToTurn: [],
+			locAcc: []
 		}
 
 		builtBoard = Enum.map(0..63, fn x -> %{id: x, row: div(x, 8), col: rem(x, 8), empty: true, color: nil} end)
@@ -56,7 +58,8 @@ defmodule Othello.Game do
 				turn: game.turn,
 				player1: game.player1,
 				player2: game.player2,
-				status: game.status
+				status: game.status,
+				#pcsToTurn: game.pcsToTurn
 			}
 		end
 
@@ -81,9 +84,8 @@ defmodule Othello.Game do
 				|> Map.put(:player2, userName)
 				|> Map.put(:status, "black's turn")
 
-			else	# There are two players in the game already
-				## TODO ??? ##
-				#game
+			else	# There are two players in the game already, so others can just watch
+				game
 			end
 		end
 	end
@@ -110,17 +112,28 @@ defmodule Othello.Game do
 					"black"
 			end
 
-			pcsToTurn = findPcsToFlip(game, id)
+			game = findPcsToFlip(game, id)
 
 			# There is somehting to turn based on the valid move
-			if (length(pcsToTurn) != 0) do
-				newBoard = Enum.map(pcsToTurn, fn x ->
-					Map.put(Enum.at(game.board, x), :color, game.turn)
-				end)
+			if (length(game.pcsToTurn) != 0) do
 
+				newBoard = Enum.map(0..63, fn x -> 
+					if contain(game.pcsToTurn, x) do
+						temp = Enum.at(game.board, x)
+						|> Map.put(:color, game.turn)
+						|> Map.put(:empty, false)
+						#List.replace_at(game.board, x, temp)
+					else
+						Enum.at(game.board, x)
+					end
+				end)
 				game
+					|> Map.put(:pcsToTurn, [])
 					|> Map.put(:board, newBoard)
-					|> Map.put(:status, game.turn <> "'s turn")
+					|> Map.put(:status, nextTurn <> "'s turn")
+					|> Map.put(:turn, nextTurn)
+			else
+				game
 			end
 
 			# if (noMove(game, nextTurn)) do
@@ -141,6 +154,13 @@ defmodule Othello.Game do
 		end
 	end
 
+	# Whether the list contains x
+	defp contain(list, x) do
+		List.foldl(list, false, fn d, acc -> 
+			acc || (d == x)
+		end)
+	end
+
 	def noMove(game, gTurn) do
 		game
 		|> Map.put(:turn, gTurn)
@@ -156,66 +176,28 @@ defmodule Othello.Game do
 		true
 	end
 
-	# Given the cell id, finds the ids of pieces to flip
-	# game, id --> list of pieces' id to turn
+	# Game, id --> Game
 	def findPcsToFlip(game, id) do
-		# Based on clicked id, find the row and column
-		rowC = Enum.at(game.board, id).row
-		colC = Enum.at(game.board, id).col
+	rowC = Enum.at(game.board, id).row
+	colC = Enum.at(game.board, id).col	
 
-    pcsToTurn = []  # Pieces to turn
-
-		# check four directions for pieces to flip (not empty and the color is opposite to this one)
-
-		# Check left neighbor
-		if (colC - 1 >= 0 && Enum.at(game.board, rcToId(rowC, colC - 1)).color != game.turn) do
-			pcsToTurn = checkDirect(game, rowC, colC - 2, "left", pcsToTurn)
-		end
-
-		# Check right neighbor
-		if (colC + 1 < 8 && Enum.at(game.board, rcToId(rowC, colC + 1)).color != game.turn) do
-			pcsToTurn = checkDirect(game, rowC, colC + 2, "right", pcsToTurn)
-		end
-
-		# Check top neighbor
-		if (rowC - 1 >= 0 && Enum.at(game.board, rcToId(rowC - 1, colC)).color != game.turn) do
-			pcsToTurn = checkDirect(game, rowC - 2, colC, "top", pcsToTurn)
-		end
-
-		# Check bottom neighbor
-		if (rowC + 1 < 8 && Enum.at(game.board, rcToId(rowC + 1, colC)).color != game.turn) do
-			pcsToTurn = checkDirect(game, rowC + 2, colC, "bottom", pcsToTurn)
-		end
-
-		# Check top-left neighbor
-		if (colC - 1 >= 0 && rowC - 1 >= 0
-			&& Enum.at(game.board, rcToId(rowC - 1, colC - 1)).color != game.turn) do
-
-			pcsToTurn = checkDirect(game, rowC - 2, colC - 2, "top-left", pcsToTurn)
-		end
-
-		# Check top-right neighbor
-		if (colC + 1 < 8 && rowC - 1 >= 0
-			&& Enum.at(game.board, rcToId(rowC - 1, colC + 1)).color != game.turn) do
-
-			pcsToTurn = checkDirect(game, rowC - 2, colC + 2, "top-right", pcsToTurn)
-		end
-
-		# Check bottom-left neighbor
-		if (colC - 1 >= 0 && rowC + 1 < 8
-			&& Enum.at(game.board, rcToId(rowC + 1, colC - 1)).color != game.turn) do
-
-			pcsToTurn = checkDirect(game, rowC + 2, colC - 2, "bottom-left", pcsToTurn)
-		end
-
-		# Check bottom-right neighbor
-		if (colC + 1 < 8 && rowC + 1 < 8
-			&& Enum.at(game.board, rcToId(rowC + 1, colC + 1)).color != game.turn) do
-
-			pcsToTurn = checkDirect(game, rowC + 2, colC + 2, "top-left", pcsToTurn)
-		end
-
-		pcsToTurn
+	game
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC, colC - 1, 0, -1)		# left direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])	
+	|> checkDirect(rowC, colC + 1, 0, 1)  		# right direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC - 1, colC, -1, 0)		# top direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC + 1, colC, 1, 0)		# bottom direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC - 1, colC - 1, -1, -1)	# top-left direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC - 1, colC + 1, -1, 1)	# top-right direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC + 1, colC - 1, 1, -1)	# bottom-left direction
+	|> Map.put(:locAcc, [rcToId(rowC, colC)])
+	|> checkDirect(rowC + 1, colC + 1, 1, 1)	# bottom-right direction
 	end
 
 	# Given row and column of the board, calculates and retrurns the corresponding id
@@ -223,153 +205,35 @@ defmodule Othello.Game do
 		column + (row * 8)
 	end
 
-	# Case of getting out of bounds
-	#defp checkDirect(game, row, col) do
-	#	[]
-	#end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the cells to the left until finds the match, out of
-	# bounds or empty cell is encountered
-	defp checkDirect(game, row, col, "left", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			col >= 0 && col < 8 ->
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
-				else
-					checkDirect(game, row, col - 1, "left", pcsToTurn ++ [rcToId(row, col)])
-				end
-			true ->
-				[]
-		end
+	defp validRC(row, col) do
+		row >= 0 && row < 8 && col >= 0 && col < 8
 	end
 
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the cells to the right until finds the match, out of
-	# bounds or empty cell is encountered
-	defp checkDirect(game, row, col, "right", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			col >= 0 && col < 8 ->
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
+	defp checkDirect(game, row, col, rOffset, cOffset) do
+		if validRC(row, col) do
+			if Enum.at(game.board, rcToId(row, col)).color == game.turn do
+			# SAME COLOR --> RETURN
+				if (length(game.locAcc) >= 2) do
+					game
+					|> Map.put(:pcsToTurn, game.pcsToTurn ++ game.locAcc)
+					|> Map.put(:locAcc, [])
 				else
-					checkDirect(game, row, col + 1, "right", pcsToTurn ++ [rcToId(row, col)])
+					game
+					|> Map.put(:locAcc, [])
 				end
-			true ->
-				[]
-		end
-	end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the cells above until finds the match, out of bounds
-	# or empty cell is encountered
-	defp checkDirect(game, row, col, "top", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			row >= 0 && row < 8 ->
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
+			else
+				if Enum.at(game.board, rcToId(row, col)).color == nil do
+					# CELL EMPTY
+					game
+					|> Map.put(:locAcc, [])
 				else
-					checkDirect(game, row - 1, col, "top", pcsToTurn ++ [rcToId(row, col)])
+					# CELL NOT SAME COLOR
+					checkDirect(Map.put(game, :locAcc, game.locAcc ++ [rcToId(row, col)]), row + rOffset, col + cOffset, rOffset, cOffset)
 				end
-			true ->
-				[]
-		end
-	end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the cells below until finds the match, out of bounds
-	# or empty cell is encountered
-	defp checkDirect(game, row, col, "bottom", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			row >= 0 && row < 8 ->	# valid row
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
-				else
-					checkDirect(game, row + 1, col, "bottom", pcsToTurn ++ [rcToId(row, col)])
-				end
-			true ->
-				[]
-		end
-	end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the top-left cells until finds the match, out of bounds
-	# or empty cell is encountered
-	defp checkDirect(game, row, col, "top-left", pcsToTurn) do
-		IO.inspect(game)
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			row >= 0 && row < 8 && col >= 0 && col < 8 ->	# valid
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
-				else
-					checkDirect(game, row - 1, col - 1, "top-left", pcsToTurn ++ [rcToId(row, col)])
-				end
-			true ->
-				[]
-		end
-	end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the top-right cells until finds the match, out of bounds
-	# or empty cell is encountered
-	defp checkDirect(game, row, col, "top-right", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			row >= 0 && row < 8 && col >= 0 && col < 8 ->	# valid
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
-				else
-					checkDirect(game, row - 1, col + 1, "top-right", pcsToTurn ++ [rcToId(row, col)])
-				end
-			true ->
-				[]
-		end
-	end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the bottom-left cells until finds the match, out of bounds
-	# or empty cell is encountered
-	defp checkDirect(game, row, col, "bottom-left", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			row >= 0 && row < 8 && col >= 0 && col < 8 ->	# valid
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
-				else
-					checkDirect(game, row + 1, col - 1, "bottom-left", pcsToTurn ++ [rcToId(row, col)])
-				end
-			true ->
-				[]
-		end
-	end
-
-	# Checks if the cell at the given row and column is of the same type(color) as the current
-	# player's color. Recursively checks the top-left cells until finds the match, out of bounds
-	# or empty cell is encountered
-	defp checkDirect(game, row, col, "bottom-right", pcsToTurn) do
-		cond do
-			Enum.at(game.board, rcToId(row, col)).empty ->
-				[]
-			row >= 0 && row < 8 && col >= 0 && col < 8 ->	# valid
-				if (game.turn == Enum.at(game.board, rcToId(row, col)).color) do
-					pcsToTurn
-				else
-					checkDirect(game, row + 1, col + 1, "bottom-right", pcsToTurn ++ [rcToId(row, col)])
-				end
-			true ->
-				[]
+			end
+		else
+			game
+			|> Map.put(:locAcc, [])
 		end
 	end
 end
